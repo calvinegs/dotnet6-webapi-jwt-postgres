397bb7379753dead1471046df2d8f6efdbfb4234
service.egs <service.egs@gmail.com>
2023-03-01 16:53:44 +0800

    fix(Program.cs): CORS issue for dotnet6-webapi-jwt-postgress

--- Program.cs
+++ Program.cs
@@ -22,42 +22,52 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddDefaultTokenProviders();
 
 builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
 
-    })
-    .AddJwtBearer(options =>
-    {
-        // 當驗證失敗時，回應標頭會包含 WWW-Authenticate 標頭，這裡會顯示失敗的詳細錯誤原因
-        options.IncludeErrorDetails = true; // 預設值為 true，有時會特別關閉
+})
+.AddJwtBearer(options =>
+{
+    // 當驗證失敗時，回應標頭會包含 WWW-Authenticate 標頭，這裡會顯示失敗的詳細錯誤原因
+    options.IncludeErrorDetails = true; // 預設值為 true，有時會特別關閉
 
-        options.TokenValidationParameters = new TokenValidationParameters
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        // 透過這項宣告，就可以從 "NAME" 取值
+        NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier",
+        // 透過這項宣告，就可以從 "Role" 取值，並可讓 [Authorize] 判斷角色
+        RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
+
+        // 驗證 Issuer (一般都會)
+        ValidateIssuer = true,
+        ValidIssuer = _configuration.GetValue<string>("JwtSettings:ValidIssuer"),
+
+        // 驗證 Audience (通常不太需要)
+        ValidateAudience = false,
+        //ValidAudience = = _configuration.GetValue<string>("JwtSettings:ValidAudience"),
+
+        // 驗證 Token 的有效期間 (一般都會)
+        ValidateLifetime = true,
+
+        // 如果 Token 中包含 key 才需要驗證，一般都只有簽章而已
+        ValidateIssuerSigningKey = false,
+
+        // 應該從 IConfiguration 取得
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret))
+    };
+});
+
+var MyAllOrigins = "allowAll";
+builder.Services.AddCors(option => 
+    option.AddPolicy(name: MyAllOrigins, 
+        policy =>
         {
-            // 透過這項宣告，就可以從 "NAME" 取值
-            NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier",
-            // 透過這項宣告，就可以從 "Role" 取值，並可讓 [Authorize] 判斷角色
-            RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
-
-            // 驗證 Issuer (一般都會)
-            ValidateIssuer = true,
-            ValidIssuer = _configuration.GetValue<string>("JwtSettings:ValidIssuer"),
-
-            // 驗證 Audience (通常不太需要)
-            ValidateAudience = false,
-            //ValidAudience = = _configuration.GetValue<string>("JwtSettings:ValidAudience"),
-
-            // 驗證 Token 的有效期間 (一般都會)
-            ValidateLifetime = true,
-
-            // 如果 Token 中包含 key 才需要驗證，一般都只有簽章而已
-            ValidateIssuerSigningKey = false,
-
-            // 應該從 IConfiguration 取得
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret))
-        };
-    });
+            policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+        }
+    )
+);
 
 // builder.Services.AddAuthorization();
 builder.Services.AddControllers();
@@ -95,7 +105,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors(MyAllOrigins);
 app.UseAuthentication();
 app.UseAuthorization();
 
